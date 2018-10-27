0c44c3ba298a0a374e01704157ed861a7cad4b5f
bettyES <betty.schirrmeister@gmail.com>
2018-10-27 11:00:11 +0100

    minutes5 implemented

--- src/main/scala/BerlinClock.scala
+++ src/main/scala/BerlinClock.scala
@@ -6,7 +6,6 @@ object BerlinClock {
     var n = "02:00:00"
     var ClockActual = new BClock(n)
 
-
   }
 
 }
@@ -23,6 +22,10 @@ class BClock(time: String){
   var hh5: Int = hh / 5
   var hh1 = hh % 5
 
+  var mm = timesComplete(1).toInt
+  var mm5: Int = mm / 5
+  var mm1 = mm % 5
+
   for (x <- 0 to hours5.length - 1) {
     if (x < hh5) {
       hours5(x) = "R"
@@ -30,11 +33,22 @@ class BClock(time: String){
       hours5(x) = "O"
     }
   }
+
   for(x<-0 to hours1.length-1) {
     if (x < hh1) {
       hours1(x) = "R"
     } else {
       hours1(x) = "O"
+    }
+  }
+
+  for (x <- 0 to minutes5.length - 1) {
+    if (x < mm5 && x % 3 == 0) {
+      minutes5(x) = "Y"
+    } else if (x < mm5) {
+      minutes5(x) = "R"
+    } else {
+      minutes5(x) = "O"
     }
   }
 }
--- src/test/scala/BerlinClockTest.scala
+++ src/test/scala/BerlinClockTest.scala
@@ -1,10 +1,11 @@
 import org.scalatest.FunSuite
 
 class BerlinClockTest extends FunSuite {
-  test("tests 12 am"){
+  test("tests 12 am") {
     var c0 = new BClock("00:00:00")
-    assert(c0.hours5(0)==="O")
-    assert(c0.hours1(0)=="O")
+    assert(c0.hours5(0) === "O")
+    assert(c0.hours1(0) === "O")
+    assert(c0.minutes5(0) === "O")
   }
 
 }
